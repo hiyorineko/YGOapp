54fa7e73315a3ed94d3e32be670ce36ae97b212b
hiyoriaya <hiyoriaya@gmail.com>
2016-02-01 15:04:12 +0900

    hiiii

--- app/src/main/java/com/example/hiyoriaya/ygoapp/MainActivity.java
+++ app/src/main/java/com/example/hiyoriaya/ygoapp/MainActivity.java
@@ -1,31 +1,35 @@
 package com.example.hiyoriaya.ygoapp;
 
-import android.app.FragmentManager;
+import android.support.v4.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.WindowManager;
 import android.widget.Toast;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity implements OnListChangeListener {
+public class MainActivity extends FragmentActivity implements OnListChangeListener {
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager)findViewById(R.id.pager);
+        viewPager.setAdapter(new MyFragmentStatePagerAdapter(getSupportFragmentManager()));
     }
 
     @Override
     public void onListSelectedChanged(String s) {
-        FragmentManager fragmentManager = getFragmentManager();
-        ContentFragment cf1 =(ContentFragment)fragmentManager.findFragmentByTag("contentFragment");
-        if(cf1==null || !cf1.isVisible()){
-            Toast.makeText(getBaseContext(),s,Toast.LENGTH_SHORT).show();
-        }else{
-            cf1.setText(s);
-        }
+        //FragmentManager fragmentManager = getFragmentManager();
+        //ContentFragment cf1 =(ContentFragment)fragmentManager.findFragmentByTag("contentFragment");
+        //if(cf1==null || !cf1.isVisible()){
+        //    Toast.makeText(getBaseContext(),s,Toast.LENGTH_SHORT).show();
+        //}else{
+        //    cf1.setText(s);
+        //}
     }
-
-
 }
---
+++ app/src/main/java/com/example/hiyoriaya/ygoapp/MyFragmentStatePagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.hiyoriaya.ygoapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by hiyorineko on 2016/02/01.
+ */
+public class MyFragmentStatePagerAdapter extends FragmentStatePagerAdapter {
+    public MyFragmentStatePagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i){
+        switch(i){
+            case 0:
+                return new CalcFragment();
+            case 1:
+                return new RecordFragment();
+            case 2:
+                return new ListFragment();
+        }
+    }
+
+    @Override
+    public int getCount(){
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        return "Page"+position;
+    }
+}
