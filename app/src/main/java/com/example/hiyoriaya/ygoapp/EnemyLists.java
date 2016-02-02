b671a2f390ed0ec26913109c014bdb07ceab8a27
hiyoriaya <hiyoriaya@gmail.com>
2016-02-02 13:47:35 +0900

    2/2 mu-mumumu

--- app/src/main/java/com/example/hiyoriaya/ygoapp/EnemyLists.java
+++ app/src/main/java/com/example/hiyoriaya/ygoapp/EnemyLists.java
@@ -1,8 +1,10 @@
 package com.example.hiyoriaya.ygoapp;
 
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,18 +12,23 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by hiyorineko on 2016/02/01.
  */
 public class EnemyLists extends Fragment implements View.OnClickListener{
 
-    ListView enemyList;
+    static ListView enemyList;
     Button addDuelist;
+    List<String> enemynames;/** id,Name.duelkaisuu */
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_enemylists,container,false);
         findView(v);
+        loaddata();
         return v;
     }
 
@@ -31,12 +38,12 @@ public class EnemyLists extends Fragment implements View.OnClickListener{
     }
 
     public void findView(View v){
+        enemynames= new ArrayList<String>();
         enemyList = (ListView)v.findViewById(R.id.enemyLists);
         enemyList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //RecordFragmentに飛ぶ
-
+                //RecordFragmentに飛ぶ(idを投げる)
             }
         });
         addDuelist = (Button)v.findViewById(R.id.addduelist);
@@ -45,6 +52,12 @@ public class EnemyLists extends Fragment implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
+        //EnemyListを追加するFragment or 処理
+        DialogFragment newFragment = new AddDuelistDialog();
+        newFragment.show(getFragmentManager(),"");
+    }
 
+    public void loaddata(){
+        //保存されている敵データを読み込む。
     }
 }
