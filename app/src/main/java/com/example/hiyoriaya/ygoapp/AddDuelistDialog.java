8ebea24dfc59256eb440c398077de81870f5a38b
hiyoriaya <hiyoriaya@gmail.com>
2016-02-02 13:49:03 +0900

    2/2 mu-mumumu

---
+++ app/src/main/java/com/example/hiyoriaya/ygoapp/AddDuelistDialog.java
@@ -0,0 +1,47 @@
+package com.example.hiyoriaya.ygoapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+/**
+ * Created by hiyorineko on 2016/02/02.
+ */
+public class AddDuelistDialog extends DialogFragment{
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View content = inflater.inflate(R.layout.adddialog_setting,null);
+        builder.setView(content);
+        builder.setMessage("デュエリスト追加").setNegativeButton("追加", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                EditText et = (EditText) content.findViewById(R.id.inputduelistname);
+                String input = et.getText().toString();
+                try {
+                    OutputStream out = openFileOutput("enemies.txt", MODE_APPEND);
+                    PrintWriter writer = new PrintWriter(new OutputStreamWriter(out,"UTF-8"));
+                    writer.append(input);
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return builder.create();
+    }
+}
